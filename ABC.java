6d7ee6c8308033e91a97e6cabc883838d78776b3
Sujit456 <101627940+Sujit456@users.noreply.github.com>
2022-03-16 14:57:00 +0530

    Add files via upload

---
+++ ABC.java
@@ -0,0 +1,6 @@
+class ABCDERT{
+   public static void main(String ar[])
+       {System.out.println("Name-Sujit Gadekar");
+	System.out.println("College- SIT Lonavala");
+	System.out.println("Branch- Electrical");
+}}
